c9fbfb267c6c54cacc1fe21db9d334974d118839
Malcolm Deck <malcolmdeck@gmail.com>
2017-02-26 18:07:24 -0800

    Added a button

--- app/src/main/java/com/games/malcolm/graphgame/MainActivity.java
+++ app/src/main/java/com/games/malcolm/graphgame/MainActivity.java
@@ -2,12 +2,26 @@ package com.games.malcolm.graphgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.click_button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Yay!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

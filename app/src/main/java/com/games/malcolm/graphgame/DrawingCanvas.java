12a56b13184545b113ac12ce5b7e8c2a46a72f9a
Malcolm Deck <malcolmdeck@gmail.com>
2017-02-26 18:53:24 -0800

    Beginnings of drawing

---
+++ app/src/main/java/com/games/malcolm/graphgame/DrawingCanvas.java
@@ -0,0 +1,20 @@
+package com.games.malcolm.graphgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by Malcolm on 2/26/2017.
+ */
+
+public class DrawingCanvas extends View {
+    public DrawingCanvas(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}

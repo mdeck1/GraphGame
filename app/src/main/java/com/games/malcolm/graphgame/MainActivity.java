99685c93bc3192c2be2ca670994ca32ba41dadd4
Malcolm Deck <malcolmdeck@gmail.com>
2017-05-20 23:29:01 -0700

    -added sign-out -rearranged main screen -added current user information -began logging failed sign in

--- app/src/main/java/com/games/malcolm/graphgame/MainActivity.java
+++ app/src/main/java/com/games/malcolm/graphgame/MainActivity.java
@@ -6,10 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.ActionCodeResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -22,10 +28,13 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 123;
 
-    private Button mSignInButton;
     private Button mNextLevelButton;
     private Button mSelectLevelButton;
     private Button mLevelEditorButton;
+    private TextView mCurrentUser;
+    private Button mSignInButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +43,32 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate()");
 
-        mSignInButton = (Button) findViewById(R.id.button_signin);
         mNextLevelButton = (Button) findViewById(R.id.button_next_level);
         mSelectLevelButton = (Button) findViewById(R.id.button_select_level);
         mLevelEditorButton = (Button) findViewById(R.id.button_edit_level);
+        mCurrentUser = (TextView) findViewById(R.id.text_user);
+        mSignInButton = (Button) findViewById(R.id.button_signin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        updateCurrentUser();
 
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-                        .setProviders(Arrays.asList(
-                                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                        .build(), RC_SIGN_IN);
+                if (mAuth.getCurrentUser() == null) {
+                    startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(), RC_SIGN_IN);
+                } else {
+                    mAuth.signOut();
+                    updateCurrentUser();
+                }
             }
         });
+
 
         mNextLevelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,5 +98,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == ResultCodes.OK) {
+                updateCurrentUser();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    Log.w(TAG, "User pressed back button");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Log.w(TAG, "No network during sign-in");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Log.e(TAG, "Unknown error during sign-in");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void updateCurrentUser() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            mCurrentUser.setText("");
+            mSignInButton.setText(R.string.button_signin);
+        } else {
+            mCurrentUser.setText("Signed-in with: " + user.getDisplayName());
+            mSignInButton.setText(R.string.button_signout);
+        }
     }
 }
